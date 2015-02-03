f7fef9e18aabffd07b44ecfa26617d9d36f2de66
“Dolores <doloresmtz@gmail.com>
2015-02-02 21:25:34 -0600

    Initial Commit

---
+++ SocialPeople/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  SocialPeople
+//
+//  Created by SantiagoDls on 02/02/15.
+//  Copyright (c) 2015 ConstruApps. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ SocialPeople/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  SocialPeople
+//
+//  Created by SantiagoDls on 02/02/15.
+//  Copyright (c) 2015 ConstruApps. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
